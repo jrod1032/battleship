9f6094c1455350fb2d9cfdd895b350f235e9f72e
Jarrod Powell <jpowell1032@gmail.com>
2018-06-19 16:06:35 -0700

    Optimize Board rendering logic
    refactor component to class to use shouldComponent lifecycle method

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,36 +4,52 @@ import PropTypes from 'prop-types';
 const rows = ['A','B','C','D','E','F','G','H','I','J']
 const columns = [1,2,3,4,5,6,7,8,9,10];
 
-const Board = (props) => {
-  const board = props.boardType === 'playerBoard' ? props.playerBoard : props.enemyBoard;
-  return (
-  <div className={props.className}
-       onMouseEnter={props.getCursorOnEnter}>
-    <table>
-      <thead>
-        <tr className="boardOuter">
-        <th></th>
-        {columns.map(column=> <th key={column}>{column}</th>)}
-        </tr> 
-      </thead>
-      <tbody>
-        {board.map( (row, rowIdx) => {
-          return <tr key={rowIdx}> <th className="boardOuter">{rows[rowIdx]}</th>
-          {columns.map((col, colIdx) => {
-            return <td
-            key={colIdx}
-            className="boardCell"
-            onClick={ 
-              () => props.onCellClick(rowIdx, colIdx, props.boardType, props.gamePhase)}>
-            {board[rowIdx][colIdx]}
-            </td>
-          })}</tr>
-        })}   
-      </tbody>
-    </table>
-  </div>
-  )
-}
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.boardType === 'playerBoard') {
+      return this.props.playerBoard !== nextProps.playerBoard || this.props.gamePhase !== nextProps.gamePhase
+    } else {
+      return this.props.enemyBoard !== nextProps.enemyBoard || this.props.gamePhase !== nextProps.gamePhase
+    }
+  }
+  render() {
+    const props = this.props;
+    const board = props.boardType === 'playerBoard' ? props.playerBoard : props.enemyBoard;
+    return (
+    <div className={props.className}
+         onMouseEnter={props.getCursorOnEnter}>
+      <table>
+        <thead>
+          <tr className="boardOuter">
+          <th></th>
+          {columns.map(column=> <th key={column}>{column}</th>)}
+          </tr> 
+        </thead>
+        <tbody>
+          {board.map( (row, rowIdx) => {
+            return <tr key={rowIdx}> <th className="boardOuter">{rows[rowIdx]}</th>
+            {columns.map((col, colIdx) => {
+              return <td
+              key={colIdx}
+              className="boardCell"
+              onClick={ 
+                () => props.onCellClick(rowIdx, colIdx, props.boardType, props.gamePhase)}>
+              {board[rowIdx][colIdx]}
+              </td>
+            })}</tr>
+          })}   
+        </tbody>
+      </table>
+    </div>
+    )
+  }
+} 
+
+
+
 
 Board.PropTypes = {
   boardType: PropTypes.string.isRequired,
