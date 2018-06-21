b28bf1c5ac05df12fcdc5517fa450fb1846b3a23
Jarrod Powell <jpowell1032@gmail.com>
2018-06-21 16:48:40 -0700

    Display who won game when game ends

--- src/containers/GamePhaseContainer.js
+++ src/containers/GamePhaseContainer.js
@@ -1,19 +1,21 @@
 import { connect } from 'react-redux';
 import GamePhaseBoard from '../components/GamePhaseBoard.jsx';
+import { PLAYER_NAME  } from '../gameConstants.js';
 
-const getGamePhaseWording = (gamePhase) => {
+const getGamePhaseWording = (gamePhase, player) => {
   if (gamePhase === 'pregamePhase'){
     return 'Select Your Ships';
   } else if (gamePhase === 'battlePhase') {
     return 'Battle!'
   } else if (gamePhase === 'endGame') {
-    return 'Game Over'
+    return player === PLAYER_NAME ? `You Lose!` : 'You Win'
   }
 }
 
 const mapStateToProps = state => {
+  console.log(state)
   return {
-    gamePhase: getGamePhaseWording(state.gamePhase)
+    gamePhase: getGamePhaseWording(state.gamePhase, state.gameLogic.turn)
   }
 }
 
